9f02b022297f12f474bbd2f3bb3882432d874ab1
NOUR_PC <NOUR_PC@DESKTOP-H2GBP6G>
2026-04-27 10:39:58 +0300

    Front side Validation

--- Team_Task_Manager/ViewModels/User/UserViewModel.cs
+++ Team_Task_Manager/ViewModels/User/UserViewModel.cs
@@ -1,12 +1,24 @@
-﻿using Team_Task_Manager.Models.Entities.Role;
+﻿using System.ComponentModel.DataAnnotations;
+using Team_Task_Manager.Models.Entities.Role;
 
 namespace Team_Task_Manager.ViewModels.User
 {
     public class UserViewModel
     {
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
         public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Role is required")]
         public long SelectedRoleId { get; set; }
     }
 }
